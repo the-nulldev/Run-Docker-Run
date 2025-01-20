4f72c014d91c5e0639db0565f32dc0a960f8d545
Ephantus Mwangi <62132661+the-nulldev@users.noreply.github.com>
2025-01-20 10:44:57 +0300

    Bug fixes.

--- Run-Docker-Run/stage4/src/Task.kt
+++ Run-Docker-Run/stage4/src/Task.kt
@@ -1,7 +1,7 @@
 fun main() {
     // Provide the absolute path to the Dockerfile
     // Replace the string below with the actual path to your Dockerfile
-    val dockerfilePath = "/home/devnull/Downloads/hyper-web-app/Dockerfile"
+    val dockerfilePath = "/absolute/path/to/hyper-web-app/Dockerfile"
 
     // for Windows use the following format
     // val dockerfilePath = "C:\\Users\\<USERNAME>\\path\\to\hyper-web-app\\Dockerfile"
